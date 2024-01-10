dfd5ab50bdbf51d86800399682cc44653a0a8645
NotAliAhmad <ali.ahmad.contact@gmail.com>
2024-01-10 14:22:31 -0500

    adding leet code folder and go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/notaliahmad/dsa
+
+go 1.21.1
---
+++ leetcode/twosum/twosum.go
@@ -0,0 +1,34 @@
+package twoSum
+
+/*
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+*/
+
+func TwoSum(arr []int, target int) []int {
+	var newArr []int
+	var currentIndex int
+	for i := 0; i < len(arr); i++ {
+		currentIndex = i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[currentIndex]+arr[j] == target {
+				newArr = append(newArr, i, j)
+				return newArr
+			}
+		}
+	}
+	return newArr
+}
